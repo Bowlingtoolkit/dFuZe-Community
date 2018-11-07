3362070d37a088ad746b5694bba77ad45629275a
Discord.js Arabic Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2018-11-07 18:27:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1631,7 +1631,7 @@ client.on('guildMemberAdd', member => {
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const logChannel = member.guild.channels.find(channel => channel.name === "💬welcome");
-    logChannel.send(`${inviter.tag}`);
+    logChannel.send(`Invited by: ${inviter}`);
   });
 });
 
