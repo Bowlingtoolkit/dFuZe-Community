25a18272d94b519d5c88498f9f0c17468f61dea7
Discord.js Arabic Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2018-11-09 08:32:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1729,46 +1729,39 @@ client.on('guildMemberAdd', member => {
 });
 
 
+const coolDown = new Set();
 client.on('message', message => {
-const talkedRecently = new Set();
+  
+      if (message.content.startsWith("رابط")) {
+        if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your invite :yen: link refreshes in \`\`1 Day\`\`.**`);
 
-    if (message.content.startsWith("رابط")) {
-  // First, this must be at the top level of your code, **NOT** in any event!
+    message.channel.createInvite({
+  
+          thing: true,
+  
+          maxUses: 5,
+  
+          maxAge: 86400
+  
+      }).then(invite =>
+  
+        message.author.sendMessage(invite.url)
+  
+      )
+  
+    message.channel.send("**تم ارسال الرابط برسالة خاصة**")   .then(() => {     
+      coolDown.add(message.author.id);
+  });
+  
+  
+  message.author.send(`**مدة الرابط : يـوم
+  عدد استخدامات الرابط : 5**`)
+  
+      }
 
-    if (talkedRecently.has(message.author.id)) {
-            message.channel.send("Wait 24h minute before getting typing this again. - " + message.author);
-    } else {
-
-           // the user can type the command ... your command code goes here :)
-
-        // Adds the user to the set so that they can't talk for a minute
-        talkedRecently.add(message.author.id);
-        setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(message.author.id);
-        }, 86400000);
-    }
-  message.channel.createInvite({
-
-        thing: true,
-
-        maxUses: 5,
-
-        maxAge: 86400
-
-    }).then(invite =>
-
-      message.author.sendMessage(invite.url)
-
-    )
-
-  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
-
-message.author.send(`**مدة الرابط : يـوم
-عدد استخدامات الرابط : 5**`)
-
-    }
-
-});
-
+      setTimeout(() => {
+        coolDown.remove(message.author.id);
+     },86400000);
+     
+  });  
 client.login(process.env.BOT_TOKEN);
